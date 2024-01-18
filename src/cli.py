66d6654cd68963597fbb3f476d6ce569b58dab12
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-01-17 21:31:58 -0500

    Refactor to not have a global parser variable

--- src/cli.py
+++ src/cli.py
@@ -8,8 +8,9 @@ def __check_attr(attr: str) -> Attribute:
     try:
         return Attribute[attr]
     except KeyError:
-        global_parser.error(f"{attr} is not a valid attribute."
-                            f"\nValid attributes are: {', '.join(Attribute.__members__)}")
+        raise MonitorBossError(
+            f"{attr} is not a valid attribute."
+            f"\nValid attributes are: {', '.join(Attribute.__members__)}")
 
 
 def __check_mon(mon: str) -> int:
@@ -17,8 +18,9 @@ def __check_mon(mon: str) -> int:
     try:
         return int(mon)
     except ValueError:
-        global_parser.error(f"{mon} is not a valid monitor."
-                            f"\nValid monitors are: {', '.join(monitor_names)}, or an ID number")
+        raise MonitorBossError(
+            f"{mon} is not a valid monitor."
+            f"\nValid monitors are: {', '.join(monitor_names)}, or an ID number")
 
 
 def __check_val(attr: Attribute, val: str) -> ColorPreset | InputSource | PowerMode | int:
@@ -32,41 +34,46 @@ def __check_val(attr: Attribute, val: str) -> ColorPreset | InputSource | PowerM
                 try:
                     return int(val)
                 except ValueError:
-                    global_parser.error(f"{val} is an invalid input source."
-                                        f"\nValid input sources are: {', '.join(InputSource.__members__)}"
-                                        "\nNOTE: A particular monitor will probably support only some of these values,"
-                                        " if any. Check your monitor's specs for the inputs it accepts.")
+                    raise MonitorBossError(
+                        f"{val} is an invalid input source."
+                        f"\nValid input sources are: {', '.join(InputSource.__members__)}"
+                        "\nNOTE: A particular monitor will probably support only some of these values,"
+                        " if any. Check your monitor's specs for the inputs it accepts.")
 
         case Attribute.CNT:
             try:
                 return int(val)
             except ValueError:
-                global_parser.error(f"{val} is an invalid contrast value."
-                                    f"\nValid contrast values are typically 0-100.")
+                raise MonitorBossError(
+                    f"{val} is an invalid contrast value."
+                    f"\nValid contrast values are typically 0-100.")
 
         case Attribute.LUM:
             try:
                 return int(val)
             except ValueError:
-                global_parser.error(f"{val} is an invalid luminance value."
-                                    f"\nValid luminance values are typically 0-100.")
+                raise MonitorBossError(
+                    f"{val} is an invalid luminance value."
+                    f"\nValid luminance values are typically 0-100.")
 
         case Attribute.PWR:
             # PowerMode values are lowercase; other enums are uppercase.
             try:
                 return PowerMode[val.lower()]
             except KeyError:
-                global_parser.error(f"{val} is an invalid power mode."
-                                    f"\nValid power modes are: {', '.join(map(str.upper, PowerMode.__members__))}")
+                raise MonitorBossError(
+                    f"{val} is an invalid power mode."
+                    f"\nValid power modes are: {', '.join(map(str.upper, PowerMode.__members__))}")
 
         case Attribute.CLR:
             # ColorPreset values all start with "COLOR_TEMP_".
             try:
                 return ColorPreset[f"COLOR_TEMP_{val}"]
             except KeyError:
-                global_parser.error(f"{val} is an invalid color preset."
-                                    f"""\nValid color presets are: {
-                                        ', '.join(m.removeprefix('COLOR_TEMP_') for m in ColorPreset.__members__)}""")
+                raise MonitorBossError(
+                    f"{val} is an invalid color preset."
+                    f"""\nValid color presets are: {
+                        ", ".join(m.removeprefix("COLOR_TEMP_") for m in ColorPreset.__members__)}""")
 
 
 def __list_mons(args):
@@ -86,7 +93,7 @@ def __list_mons(args):
             try:
                 caps = monitor.get_vcp_capabilities()
             except:
-                global_parser.error("could not list information for monitor #{index}.")
+                raise MonitorBossError("could not list information for monitor #{index}.")
             print(f"monitor #{index}", end="")
             for name, value in monitor_names.items():
                 if value == index:
@@ -133,34 +140,39 @@ def __tog_attr(args):
     toggle_attribute(mons, attr, val1, val2)
 
 
-global_parser = ArgumentParser(description="Boss your monitors around.")
-subparsers = global_parser.add_subparsers(title="subcommands", help="basic commands", dest="subcommand", required=True)
+def make_parser():
+    parser = ArgumentParser(description="Boss your monitors around.")
+    subparsers = parser.add_subparsers(title="subcommands", help="basic commands", dest="subcommand", required=True)
 
-list_parser = subparsers.add_parser("list", help="list all the monitors and their possible attributes")
-list_parser.set_defaults(func=__list_mons)
+    list_parser = subparsers.add_parser("list", help="list all the monitors and their possible attributes")
+    list_parser.set_defaults(func=__list_mons)
 
-get_parser = subparsers.add_parser("get", help="return the value of a given attribute")
-get_parser.set_defaults(func=__get_attr)
-get_parser.add_argument("attr", type=str.upper, help="the attribute to return")
-get_parser.add_argument("mon", type=str.upper, help="the monitor to control")
+    get_parser = subparsers.add_parser("get", help="return the value of a given attribute")
+    get_parser.set_defaults(func=__get_attr)
+    get_parser.add_argument("attr", type=str.upper, help="the attribute to return")
+    get_parser.add_argument("mon", type=str.upper, help="the monitor to control")
 
-set_parser = subparsers.add_parser("set", help="sets a given attribute to a given value")
-set_parser.set_defaults(func=__set_attr)
-set_parser.add_argument("attr", type=str.upper, help="the attribute to set")
-set_parser.add_argument("val", type=str.upper, help="the value to set the attribute to")
-set_parser.add_argument("mon", type=str.upper, nargs="+", help="the monitor(s) to control")
+    set_parser = subparsers.add_parser("set", help="sets a given attribute to a given value")
+    set_parser.set_defaults(func=__set_attr)
+    set_parser.add_argument("attr", type=str.upper, help="the attribute to set")
+    set_parser.add_argument("val", type=str.upper, help="the value to set the attribute to")
+    set_parser.add_argument("mon", type=str.upper, nargs="+", help="the monitor(s) to control")
 
-tog_parser = subparsers.add_parser("tog", help="toggles a given attribute between two given values")
-tog_parser.set_defaults(func=__tog_attr)
-tog_parser.add_argument("attr", type=str.upper, help="the attribute to toggle")
-tog_parser.add_argument("val1", type=str.upper, help="the first value to toggle between")
-tog_parser.add_argument("val2", type=str.upper, help="the second value to toggle between")
-tog_parser.add_argument("mon", type=str.upper, nargs="+", help="the monitor(s) to control")
+    tog_parser = subparsers.add_parser("tog", help="toggles a given attribute between two given values")
+    tog_parser.set_defaults(func=__tog_attr)
+    tog_parser.add_argument("attr", type=str.upper, help="the attribute to toggle")
+    tog_parser.add_argument("val1", type=str.upper, help="the first value to toggle between")
+    tog_parser.add_argument("val2", type=str.upper, help="the second value to toggle between")
+    tog_parser.add_argument("mon", type=str.upper, nargs="+", help="the monitor(s) to control")
+
+    return parser
 
 
 def run(args):
-    args = global_parser.parse_args(args)
+    parser = make_parser()
+    args = parser.parse_args(args)
+
     try:
         args.func(args)
     except MonitorBossError as err:
-        global_parser.error(err)
+        parser.error(err)
