4bb1d5e0db5f60841fc9a1ebb3a3f167d2ddbb7b
Seizure1990 <seizure1990@gmail.com>
2024-03-01 19:01:10 -0500

    converted parser to global, added function to collect help tests into a dict

--- monitorboss/cli.py
+++ monitorboss/cli.py
@@ -4,7 +4,6 @@ from pprint import PrettyPrinter
 from config import *
 from impl import *
 
-
 def __check_attr(attr: str) -> Attribute:
     try:
         return Attribute[attr]
@@ -151,40 +150,42 @@ def __tog_attr(args, cfg: Config):
     toggle_attribute(mons, attr, val1, val2)
 
 
-def __make_parser():
-    parser = ArgumentParser(description="Boss your monitors around.")
-    subparsers = parser.add_subparsers(title="subcommands", help="basic commands", dest="subcommand", required=True)
+parser = ArgumentParser(description="Boss your monitors around.")
+subparsers = parser.add_subparsers(title="subcommands", help="basic commands", dest="subcommand", required=True)
 
-    text = "list all the monitors and their possible attributes"
-    list_parser = subparsers.add_parser("list", help=text, description=text)
-    list_parser.set_defaults(func=__list_mons)
+text = "list all the monitors and their possible attributes"
+list_parser = subparsers.add_parser("list", help=text, description=text)
+list_parser.set_defaults(func=__list_mons)
 
-    text = "return the value of a given attribute"
-    get_parser = subparsers.add_parser("get", help=text, description=text)
-    get_parser.set_defaults(func=__get_attr)
-    get_parser.add_argument("attr", type=str.upper, help="the attribute to return")
-    get_parser.add_argument("mon", type=str.upper, help="the monitor to control")
+text = "return the value of a given attribute"
+get_parser = subparsers.add_parser("get", help=text, description=text)
+get_parser.set_defaults(func=__get_attr)
+get_parser.add_argument("attr", type=str.upper, help="the attribute to return")
+get_parser.add_argument("mon", type=str.upper, help="the monitor to control")
 
-    text = "sets a given attribute to a given value"
-    set_parser = subparsers.add_parser("set", help=text, description=text)
-    set_parser.set_defaults(func=__set_attr)
-    set_parser.add_argument("attr", type=str.upper, help="the attribute to set")
-    set_parser.add_argument("val", type=str.upper, help="the value to set the attribute to")
-    set_parser.add_argument("mon", type=str.upper, nargs="+", help="the monitor(s) to control")
+text = "sets a given attribute to a given value"
+set_parser = subparsers.add_parser("set", help=text, description=text)
+set_parser.set_defaults(func=__set_attr)
+set_parser.add_argument("attr", type=str.upper, help="the attribute to set")
+set_parser.add_argument("val", type=str.upper, help="the value to set the attribute to")
+set_parser.add_argument("mon", type=str.upper, nargs="+", help="the monitor(s) to control")
 
-    text = "toggles a given attribute between two given values"
-    tog_parser = subparsers.add_parser("tog", help=text, description=text)
-    tog_parser.set_defaults(func=__tog_attr)
-    tog_parser.add_argument("attr", type=str.upper, help="the attribute to toggle")
-    tog_parser.add_argument("val1", type=str.upper, help="the first value to toggle between")
-    tog_parser.add_argument("val2", type=str.upper, help="the second value to toggle between")
-    tog_parser.add_argument("mon", type=str.upper, nargs="+", help="the monitor(s) to control")
+text = "toggles a given attribute between two given values"
+tog_parser = subparsers.add_parser("tog", help=text, description=text)
+tog_parser.set_defaults(func=__tog_attr)
+tog_parser.add_argument("attr", type=str.upper, help="the attribute to toggle")
+tog_parser.add_argument("val1", type=str.upper, help="the first value to toggle between")
+tog_parser.add_argument("val2", type=str.upper, help="the second value to toggle between")
+tog_parser.add_argument("mon", type=str.upper, nargs="+", help="the monitor(s) to control")
 
-    return parser
+del text # We're done with the subparsers
+
+
+def get_help_texts():
+    return {'': parser.format_help()} | {name: subparser.format_help() for name, subparser in subparsers.choices.items()}
 
 
 def run(args=None):
-    parser = __make_parser()
     args = parser.parse_args(args)
 
     try:
