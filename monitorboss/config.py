b43ce69a5f12693458d462b3218ecc1f16f2c1c0
Seizure1990 <seizure1990@gmail.com>
2024-03-05 04:32:41 -0500

    made cfg_parser global, added __write_config function, added functions for adding and removing monitor aliases

--- monitorboss/config.py
+++ monitorboss/config.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 from dataclasses import dataclass, field
+from pathlib import Path
 
 import monitorboss as mb
 
@@ -16,7 +17,6 @@ USBC = 27 # 27 seems to be the "standard non-standard" ID for USB-C among manufa
 WAIT = 2.0 # time to wait between commands, to avoid DDC/CI latency conflicts 
 """.lstrip()
 
-
 @dataclass
 class Config:
     monitor_names: dict[str, int] = field(default_factory=dict)
@@ -24,11 +24,12 @@ class Config:
     wait_time: float = field(default_factory=float)
 
 
+cfg_parser = ConfigParser(inline_comment_prefixes="#")
+cfg_parser.optionxform = str  # case-sensitive keys TODO: get rid of this, after making MB internals case-insensitive
+
+
 def read_config(path: str | None = None) -> Config:
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
-
-    cfg_parser = ConfigParser(inline_comment_prefixes="#")
-    cfg_parser.optionxform = str # case-sensitive keys
 
     try:
         with open(path, "r", encoding="utf8") as file:
@@ -54,7 +55,39 @@ def read_config(path: str | None = None) -> Config:
     return cfg
 
 
+def __write_config(path: str | None = None):
+    """It's expected that read_config was called at some point prior to using this.
+    Otherwise, you'll just blank the config/file"""
+
+    path = path if path is not None else DEFAULT_CONF_FILE_LOC
+
+    try:
+        with open(path, "w", encoding="utf8") as file:
+            cfg_parser.write(file)
+    except:
+        raise mb.MonitorBossError(f'could not write to config file "{path}"')
+
+
+def add_monitor_alias(alias: str, monid: int, path: str | None = None):
+    path = path if path is not None else DEFAULT_CONF_FILE_LOC
+    read_config(path)
+
+    cfg_parser.set("MONITOR_NAMES", alias, str(monid))
+
+    __write_config(path)
+
+
+def remove_monitor_alias(alias: str, path: str | None = None):
+    path = path if path is not None else DEFAULT_CONF_FILE_LOC
+    read_config(path)
+
+    cfg_parser.remove_option("MONITOR_NAMES", alias)
+
+    __write_config(path)
+
+
 def reset_config(path: str | None = None):
+    #TODO: when we fully support comments in config (see issue: https://github.com/Seizure/MonitorBoss/issues/7) this should call __write_config
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
 
     try:
