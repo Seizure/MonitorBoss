7dfb59ff267e0f0b4a23813bf97ec7a2897e84b6
Seizure1990 <seizure1990@gmail.com>
2024-03-20 00:42:42 -0400

    convert config.py to use TOML instead of conf

--- monitorboss/config.py
+++ monitorboss/config.py
@@ -1,21 +1,24 @@
-from configparser import ConfigParser
+
+# from configparser import ConfigParser
 from dataclasses import dataclass, field
+
+from tomlkit import parse, dumps, dump, comment, document, table, TOMLDocument
 
 from monitorboss import MonitorBossError
 
-DEFAULT_CONF_FILE_LOC = "./conf/MonitorBoss.conf"
+DEFAULT_CONF_FILE_LOC = "./conf/MonitorBoss.toml"
 
 #  TODO: when we fully support comments in config (see issue: https://github.com/Seizure/MonitorBoss/issues/7) this should just be a prebuilt cfg_parser
-DEFAULT_CONF_CONTENT = """
-[MONITOR_NAMES]
-DEFAULT = 0
-
-[INPUT_NAMES]
-USBC = 27 # 27 seems to be the "standard non-standard" ID for USB-C among manufacturers
-
-[SETTINGS]
-WAIT = 2.0 # time in seconds to wait between commands, to avoid DDC/CI latency conflicts 
-""".lstrip()
+# DEFAULT_CONF_CONTENT = """
+# [MONITOR_NAMES]
+# DEFAULT = 0
+#
+# [INPUT_NAMES]
+# USBC = 27 # 27 seems to be the "standard non-standard" ID for USB-C among manufacturers
+#
+# [SETTINGS]
+# WAIT = 2.0 # time in seconds to wait between commands, to avoid DDC/CI latency conflicts
+# """.lstrip()
 
 @dataclass
 class Config:
@@ -24,104 +27,130 @@ class Config:
     wait_time: float = field(default_factory=float)
 
 
-cfg_parser = ConfigParser(inline_comment_prefixes="#")
-cfg_parser.optionxform = str  # case-sensitive keys TODO: get rid of this, after making MB internals case-insensitive
+# cfg_parser = ConfigParser(inline_comment_prefixes="#")
+# cfg_parser.optionxform = str  # case-sensitive keys TODO: get rid of this, after making MB internals case-insensitive
 
 
-def read_config(path: str | None = None) -> Config:
+def default_toml() -> TOMLDocument:
+    mon_names = table()
+    mon_names.add(comment("Seizure's configuration"))
+    mon_names.add("RIGHT", 0)
+    mon_names.add("MIDDLE", 1)
+    mon_names.add("LEFT", 2)
+
+    input_names = table()
+    input_names.add("USBC", 27)
+    input_names["USBC"].comment("27 seems to be the \"standard non-standard\" ID for USB-C among manufacturers")
+
+    settings = table()
+    settings.add("WAIT", 2.0)
+
+    doc = document()
+    doc.add("MONITOR_NAMES", mon_names)
+    doc.add("INPUT_NAMES", input_names)
+    doc.add("SETTINGS", settings)
+
+    return doc
+
+
+def __get_toml(path: str | None = None) -> TOMLDocument:
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
 
     try:
         with open(path, "r", encoding="utf8") as file:
-            cfg_parser.read_file(file, path)
+            content = file.read()
     except:
-        raise MonitorBossError(f'could not read config file "{path}"')
+        raise MonitorBossError(f'could not open config file for reading: "{path}"')
+
+    return parse(content)
+
+
+def get_config(path: str | None = None) -> Config:
+    path = path if path is not None else DEFAULT_CONF_FILE_LOC
+
+    doc = __get_toml(path)
 
     cfg = Config()
-
     try:
-        for key, value in cfg_parser["MONITOR_NAMES"].items():
-            cfg.monitor_names[key] = int(value)
-        for key, value in cfg_parser["INPUT_NAMES"].items():
-            cfg.input_source_names[key] = int(value)
-        cfg.wait_time = float(cfg_parser["SETTINGS"]["WAIT"])
+        for key, value in doc["MONITOR_NAMES"].items():
+            cfg.monitor_names[key] = value
+        for key, value in doc["INPUT_NAMES"].items():
+            cfg.input_source_names[key] = value
+        cfg.wait_time = doc["SETTINGS"]["WAIT"]
     except:
-        raise MonitorBossError(f'could not parse config file "{path}"')
+        raise MonitorBossError(f'could not parse config file: "{path}"')
 
     # As far as I can tell, negative numbers in python's sleep has undefined behavior, so we want to catch that
     if cfg.wait_time < 0:
-        raise MonitorBossError(f'WAIT time is set to a negative value in config file "{path}"')
+        raise MonitorBossError(f'WAIT time is set to a negative value in config file: "{path}"')
 
     return cfg
 
 
-def __write_config(path: str | None = None):
-    """It's expected that read_config was called at some point prior to using this.
-    Otherwise, you'll just blank the config/file"""
-
+def __write_toml(config: TOMLDocument, path: str | None = None):
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
 
     try:
         with open(path, "w", encoding="utf8") as file:
-            cfg_parser.write(file)
+            dump(config, file)
     except:
-        raise MonitorBossError(f'could not write to config file "{path}"')
+        raise MonitorBossError(f'could not open config file for writing: "{path}"')
 
 
 def set_monitor_alias(alias: str, monid: int, path: str | None = None):
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
-    read_config(path)
 
-    cfg_parser.set("MONITOR_NAMES", alias, str(monid))
+    doc = __get_toml(path)
 
-    __write_config(path)
+    doc["MONITOR_NAMES"][alias] = monid
+
+    __write_toml(doc, path)
 
 
 def remove_monitor_alias(alias: str, path: str | None = None):
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
-    read_config(path)
 
-    cfg_parser.remove_option("MONITOR_NAMES", alias)
+    doc = __get_toml(path)
 
-    __write_config(path)
+    doc.remove(doc["MONITOR_NAMES"][alias])
+
+    __write_toml(doc, path)
 
 
 def set_input_alias(alias: str, inputid: int, path: str | None = None):
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
-    read_config(path)
 
-    cfg_parser.set("INPUT_NAMES", alias, str(inputid))
+    doc = __get_toml(path)
 
-    __write_config(path)
+    doc["INPUT_NAMES"][alias] = inputid
+
+    __write_toml(doc, path)
 
 
 def remove_input_alias(alias: str, path: str | None = None):
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
-    read_config(path)
 
-    cfg_parser.remove_option("INPUT_NAMES", alias)
+    doc = __get_toml(path)
 
-    __write_config(path)
+    doc.remove(doc["INPUT_NAMES"][alias])
+
+    __write_toml(doc, path)
 
 
 def set_wait_time(wait: float, path: str | None = None):
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
-    read_config(path)
 
     if wait < 0:
         raise MonitorBossError(f'WAIT time can not be set to a negative value: {wait}')
 
-    cfg_parser.set("SETTINGS", "WAIT", str(wait))
+    doc = __get_toml(path)
 
-    __write_config(path)
+    doc["SETTINGS"]["WAIT"] = wait
+
+    __write_toml(doc, path)
 
 
 def reset_config(path: str | None = None):
-    #  TODO: when we fully support comments in config (see issue: https://github.com/Seizure/MonitorBoss/issues/7) this should call __write_config
     path = path if path is not None else DEFAULT_CONF_FILE_LOC
 
-    try:
-        with open(path, "w", encoding="utf8") as file:
-            file.write(DEFAULT_CONF_CONTENT)
-    except:
-        raise MonitorBossError(f'could not reset config file "{path}"')
+    __write_toml(default_toml(), path)
