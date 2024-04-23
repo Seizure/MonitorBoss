1be4660fce5e2352ee7916190642aff88e04e516
Jesse Weiman <jesse.weiman@storable.com>
2024-04-23 18:51:51 -0400

    some minor filename and variable refactoring; fixed typo in DummyVCP; fleshed out test functions and all basics of vcp_abc are now covered

--- test/test_pyddc.py
+++ test/test_pyddc.py
@@ -1,20 +1,73 @@
 import pytest
 
+
 import sys
 sys.platform = "test"
 # this is part of a demonic hack to load in a dummy VCP in the test framework
 # otherwise, Python loads unnecessary OS-specific files,
 # which can potentially cause errors if running on an otherwise unsupported OS
 # there is probably a better way to do this.
-from pyddc import VCP
+from pyddc import VCP, VCPCommand, create_vcp_com, get_vcp_com, parse_capabilities
 
 vcps = VCP.get_vcps()
+input_command = get_vcp_com(96)
+lum_command = get_vcp_com(16)
+reset_command = get_vcp_com(4)
+active_control = get_vcp_com(82)
 
-def test_check_for_cm_req():
-    v = vcps[0]
-    with pytest.raises(AssertionError):
-        v.get_vcp_feature(0)
-    with pytest.raises(AssertionError):
-        v.set_vcp_feature(0, 0)
-    with pytest.raises(AssertionError):
-        v.get_vcp_capabilities()
+
+class TestGetFeature:
+
+    vcp = vcps[0]
+
+    def test_cm_assertion(self):
+        with pytest.raises(AssertionError):
+            self.vcp.get_vcp_feature(input_command)
+
+    def test_unreadable_code(self):
+        with self.vcp as vcp:
+            with pytest.raises(TypeError):
+                vcp.get_vcp_feature(reset_command)
+
+    def test_unsupported_code(self):
+        # TODO: need to sort out expected/proper behavior for this
+        pass
+
+
+class TestSetFeature:
+
+    vcp = vcps[0]
+
+    def test_cm_assertion(self):
+        with pytest.raises(AssertionError):
+            self.vcp.set_vcp_feature(input_command, 27)
+
+    def test_unwriteable_code(self):
+        with self.vcp as vcp:
+            with pytest.raises(TypeError):
+                vcp.set_vcp_feature(active_control, 0)
+
+    def test_set_above_max(self):
+        with self.vcp as vcp:
+            with pytest.raises(ValueError):
+                vcp.set_vcp_feature(lum_command, 85)
+
+    def test_unsupported_code(self):
+        # TODO: need to sort out expected/proper behavior for this
+        pass
+
+class TestCapabilitiesFunctions:
+
+    vcp = vcps[0]
+
+    def test_cm_assertion(self):
+        with pytest.raises(AssertionError):
+            self.vcp.get_vcp_capabilities()
+
+    def test_caps_parsing(self):
+        with vcps[0] as vcp:
+            caps = parse_capabilities(vcp.get_vcp_capabilities())
+            # TODO: caps parsing is changing soon, this will break as soon as it does
+            assert caps.__str__() == "{'prot': 'monitor', 'type': 'LCD', 'model': 'DUMM13', 'cmds': [<4>], 'vcp': [<16>, <18>, <96 (<27>, <15>, <17>)>, <170 (<1>, <2>, <4>)>], 'mccs_ver': '2.1'}"
+
+
--- test/vcp_dummy.py
+++ test/vcp_dummy.py
@@ -15,7 +15,7 @@ supported_codes = {
 caps_str = "(prot(monitor)type(LCD)model(DUMM13)cmds(04)vcp(10 12 60(1B 0F 11 ) AA(01 02 04 ) )mccs_ver(2.1))"
 
 current_values = {16: 75, 96: 27, 170: 2}  # lum is 75, input is USBC(27), image orientation is 2 (whatever that means)
-max_values = {16: 80, 18: 100}
+unknown_max_values = {16: 80, 18: 100}
 
 
 class DummyVCP(VCP):
@@ -35,14 +35,14 @@ class DummyVCP(VCP):
     ) -> Optional[bool]:
         return super().__exit__(exception_type, exception_value, exception_traceback)
 
-    def _set_vcp_feature(self, code: VCPCommand, value: int):
-        code = VCPCommand.value
+    def _set_vcp_feature(self, com: VCPCommand, value: int):
+        code = com.value
         if code not in supported_codes:
             # TODO: what should happen when an unsupported VCP command is sent?
             pass
         if supported_codes[code] is None:
-            if value > max_values[code]:
-                raise ValueError(f"value of {value} exceeds code maximum of {max_values[code]} for {VCPCommand.name}")
+            if value > unknown_max_values[code]:
+                raise ValueError(f"value of {value} exceeds code maximum of {unknown_max_values[code]} for {VCPCommand.name}")
             else:
                 current_values[code] = value
         else:
@@ -52,13 +52,13 @@ class DummyVCP(VCP):
                 # TODO: What happens if an unsupported value is sent? Presumably nothing but should check
                 pass
 
-    def _get_vcp_feature(self, code: VCPCommand) -> (int, int):
-        code = VCPCommand.value
+    def _get_vcp_feature(self, com: VCPCommand) -> (int, int):
+        code = com.value
         if code not in supported_codes:
             # TODO: what should happen when an unsupported VCP command is sent?
             pass
-        if not VCPCommand.discreet:
-            maxv = max_values[code]
+        if not com.discreet:
+            maxv = unknown_max_values[code]
         else:
             maxv = 0
             # TODO: what gets sent back as max for discreet commands?
