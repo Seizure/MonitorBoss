90e17325cce2610755846daf9894c5b7184001cb
Jesse Weiman <jesse.weiman@storable.com>
2024-04-23 18:52:37 -0400

    fixing minor warnings

--- test/test_pyddc.py
+++ test/test_pyddc.py
@@ -7,7 +7,7 @@ sys.platform = "test"
 # otherwise, Python loads unnecessary OS-specific files,
 # which can potentially cause errors if running on an otherwise unsupported OS
 # there is probably a better way to do this.
-from pyddc import VCP, VCPCommand, create_vcp_com, get_vcp_com, parse_capabilities
+from pyddc import VCP, get_vcp_com, parse_capabilities
 
 vcps = VCP.get_vcps()
 input_command = get_vcp_com(96)
@@ -56,6 +56,7 @@ class TestSetFeature:
         # TODO: need to sort out expected/proper behavior for this
         pass
 
+
 class TestCapabilitiesFunctions:
 
     vcp = vcps[0]
@@ -69,5 +70,3 @@ class TestCapabilitiesFunctions:
             caps = parse_capabilities(vcp.get_vcp_capabilities())
             # TODO: caps parsing is changing soon, this will break as soon as it does
             assert caps.__str__() == "{'prot': 'monitor', 'type': 'LCD', 'model': 'DUMM13', 'cmds': [<4>], 'vcp': [<16>, <18>, <96 (<27>, <15>, <17>)>, <170 (<1>, <2>, <4>)>], 'mccs_ver': '2.1'}"
-
-
