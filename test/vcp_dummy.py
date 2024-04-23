818ecb2f1ca29befa29fbed263b73cdadef5b772
Jesse Weiman <jesse.weiman@storable.com>
2024-04-22 23:09:23 -0400

    fleshing out of dummy VCP

--- test/vcp_dummy.py
+++ test/vcp_dummy.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from collections import namedtuple
 
 from pyddc.vcp_codes import VCPCommand
-from pyddc.vcp_abc import VCP, VCPError
+from pyddc.vcp_abc import VCP, VCPError, VCPFeatureReturn
 from types import TracebackType
 from typing import List, Optional, Type
 
@@ -15,9 +15,12 @@ supported_codes[18] = None # contrast (RW/continuous)
 supported_codes[96] = [27, 15, 17] # input source code (RW/discreet); with params for USBC, dp1, and hdm1
 supported_codes[170] = [1, 2, 4] # image orientation (RO/discreet); with params for who knows what
 
+caps_str = "(prot(monitor)type(LCD)model(DUMM13)cmds(04)vcp(10 12 60(1B 0F 11 ) AA(01 02 04 ) )mccs_ver(2.1))"
 
-internal_values = {16: 75, 96: 27, 170: 2} # lum is 75, input is USBC(27), image orientation is 2 (whatever that means)
-internal_max = {16: 80, 18: 100}
+
+current_values = {16: 75, 96: 27, 170: 2} # lum is 75, input is USBC(27), image orientation is 2 (whatever that means)
+max_values = {16: 80, 18: 100}
+
 
 class DummyVCP(VCP):
     def __init__(self):
@@ -38,31 +41,36 @@ class DummyVCP(VCP):
 
     def _set_vcp_feature(self, code: VCPCommand, value: int):
         code = VCPCommand.value
-        if code in supported_codes:
-            if value is not None and value in supported_codes[code]:
-                pass
+        if code not in supported_codes:
+            # TODO: what should happen when an unsupported VCP command is sent?
+            pass
+        if supported_codes[code] is None:
+            if value > max_values[code]:
+                raise ValueError(f"value of {value} exceeds code maximum of {max_values[code]} for {VCPCommand.name}")
+            else:
+                current_values[code] = value
+        else:
+            if value in supported_codes[code]:
+                current_values[code] = value
             else:
                 # TODO: What happens if an unsupported value is sent? Presumably nothing but should check
                 pass
-        else:
-            # TODO: what should happen when an unsupported VCP command is sent?
-            pass
 
     def _get_vcp_feature(self, code: VCPCommand) -> (int, int):
         code = VCPCommand.value
-        if code in supported_codes:
-                internal[code] = value
-            else:
-                # TODO: What happens if an unsupported value is sent? Presumably nothing but should check
-                pass
-        else:
+        if code not in supported_codes:
             # TODO: what should happen when an unsupported VCP command is sent?
             pass
+        if not VCPCommand.discreet:
+            maxv = max_values[code]
+        else:
+            maxv = 0
+            # TODO: what gets sent back as max for discreet commands?
+        return VCPFeatureReturn(current_values[code], maxv)
 
-def _get_vcp_capabilities_str(self) -> str:
-        # TODO: modify this capability string to mock that of a monitor with very limited MCCS support
-        return "(prot(monitor)type(LCD)model(U2422HE)cmds(01 02 03 07 0C E3 F3)vcp(02 04 05 08 10 12 14(01 04 05 06 08 09 0B 0C) 16 18 1A 52 60(1B 0F 11 ) AA(01 02 04 ) AC AE B2 B6 C6 C8 C9 CA CC(02 0A 03 04 08 09 0D 06 ) D6(01 04 05) DC(00 03 05 ) DF E0 E1 E2(00 02 04 0C 0D 0F 10 11 13 0B 1A 14 ) E7(02 03) EA F0(09 ) F1 F2 FD)mswhql(1)asset_eep(40)mccs_ver(2.1))"
+    def _get_vcp_capabilities_str(self) -> str:
+        return caps_str
 
     @staticmethod
     def get_vcps() -> List[DummyVCP]:
-        return [DummyVCP() for i in range(3)]
+        return [DummyVCP(), DummyVCP(), DummyVCP()]
