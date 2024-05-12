9027222a1d0b61bb576d156bc869330875d6518e
Seizure <seizure1990@gmail.com>
2024-05-12 01:56:18 -0400

    cleaning out cruft, adding read functionality

--- pyddc/macos.py
+++ pyddc/macos.py
@@ -1,45 +1,35 @@
 from collections import namedtuple
-from ctypes import Union
 from dataclasses import dataclass
+from typing import Union
 from PyObjCTools import Conversion
 import time
 
 import objc
 import Foundation
-import Quartz
 
-#######
-# get list of displays
-#######
-error, display_ids_tuple, display_count = Quartz.CGGetOnlineDisplayList(16, None, None)
-if error:
-    raise OSError(f"Could not get display list. Error: {error}")
 
-# print([x for x in dir(objc) if str(x).__contains__("_C_")])
-
+# initial thread with argument metadata: https://stackoverflow.com/questions/51862518/calling-function-with-ctypes-or-pyobjc
 IOKit = Foundation.NSBundle.bundleWithIdentifier_('com.apple.framework.IOKit')
 IOKIT_functions = [("IORegistryGetRootEntry", b"II"),
                    ("IOObjectRelease", b"iI"),
-                   ("IORegistryEntryCreateIterator", b"iI*I^I", '', # this is technically a [c128] not a * but this works with less hoops. Should check back later to consider ramifications
+                   ("IORegistryEntryCreateIterator", b"iI*I^I", '',
+                    # this is technically a [c128] not a * but this works with less hoops. Should check back later to consider ramifications
                     {'arguments': {3: {'type_modifier': b'o'}}}),
                    ("IOIteratorNext", b"II"),
                    ("IORegistryEntryGetName", b"iI[128c]", '',
                     {'arguments': {1: {'c_array_delimited_by_null': True, 'type_modifier': b'N'}}}),
                    ("IORegistryEntryCreateCFProperty", b"@I@@I"),
-                   ("IORegistryEntryGetPath", b"iI*[512c]"), # this is technically a [c128] not a * but this works with less hoops. Should check back later to consider ramifications
+                   ("IORegistryEntryGetPath", b"iI*[512c]"),
+                   # this is technically a [c128] not a * but this works with less hoops. Should check back later to consider ramifications
                    ("IOAVServiceCreateWithService", b"@@I"),
-                   ("IOAVServiceWriteI2C", b"i@II^[I]I"), # https://gist.github.com/alin23/531151c49e013554e6ca2186cef3fa90
-                   ("IOAVServiceReadI2C", b"i@II^[I]I", '', # https://gist.github.com/alin23/531151c49e013554e6ca2186cef3fa90
-                    {'arguments': {3: {'type_modifier': b'o', 'c_array_of_fixed_length': 12}}})]
+                   ("IOAVServiceWriteI2C", b"i@II@I", '',
+                    {'arguments': {4: {'type_modifier': b'n'}}}),
+                   # https://gist.github.com/alin23/531151c49e013554e6ca2186cef3fa90
+                   ("IOAVServiceReadI2C", b"i@II^[I]I", '',
+                    # https://gist.github.com/alin23/531151c49e013554e6ca2186cef3fa90
+                    {'arguments': {3: {'type_modifier': b'o', 'c_array_of_fixed_length': 15}}})]
 objc.loadBundleFunctions(IOKit, globals(), IOKIT_functions)
 
-# constants = [("KERN_SUCCESS", b"i"),
-#                    ("kIOMasterPortDefault", b"i"),
-#                    ("kIORegistryIterateRecursively", b"i")]
-#
-# objc.loadBundleVariables(IOKit, globals(), constants, skip_undefined=False)
-
-# objc.loadBundleVariables could be used to load constants?? But doesn't seem to be working
 KERN_SUCCESS = 0  # IOKit constant
 kIOMasterPortDefault = 0  # IOKit constant
 kIORegistryIterateRecursively = 1  # IOKit constant
@@ -47,8 +37,10 @@ IO_OBJECT_NULL = 0  # IOKit constant
 MACH_PORT_NULL = 0  # A constant, don't know what framework but probably IOKit?
 kCFAllocatorDefault = None  # NULL. A constant from Core Foundation
 kIOServicePlane = "IOService"  # IOKit constant
-ARM64_DDC_DATA_ADDRESS = 0x51 # defined in Arm64DDC.swift in MonitorControl project
-ARM64_DDC_7BIT_ADDRESS = 0x37 # defined in Arm64DDC.swift in MonitorControl project
+ARM64_DDC_DATA_ADDRESS = 0x51  # defined in Arm64DDC.swift in MonitorControl project
+ARM64_DDC_7BIT_ADDRESS = 0x37  # defined in Arm64DDC.swift in MonitorControl project
+
+read_buffer_size = 15  # have seen multiple decisions on this, what should it be?
 
 
 @dataclass
@@ -80,39 +72,68 @@ class Arm64Service:
     strserviceDetails: IOregService = None
     strmatchScore: int = None
 
+
 def checksum(chk: int, data: [int], start: int, end: int) -> int:
     chkd = chk
-    for i in range(start, end+1):
+    for i in range(start, end + 1):
         chkd ^= data[i]
     return chkd
 
 
+def read(ioavservice,
+         command: int,
+         writeSleepTime: float = 0.01,
+         numOfWriteCycles: int = 2,
+         readSleepTime: float = 0.05,
+         numOfRetryAttempts: int = 4,
+         retrySleepTime: int = 0) -> Union[tuple[int, int], None]:
+    send = [command]
 
-def performDDCCommunication(ioavservice, send: [int], writeSleepTime: float = 0.01, numOfWriteCycles: int = 2, readSleepTime: float = 0.05, numOfRetryAttempts: int = 4, retrySleepTime: int = 0) -> Union[int, None]
+    success, reply = performDDCCommunication(ioavservice, send, True, writeSleepTime, numOfWriteCycles, readSleepTime,
+                                         numOfRetryAttempts, retrySleepTime)
+    print('reply', list(reply))
+    if success:
+        val_max = reply[6] * 256 + reply[7]
+        val_cur = reply[8] * 256 + reply[9]
+        return val_cur, val_max
+    else:
+        return None
+
+
+def performDDCCommunication(ioavservice, send: [int], read_reply: bool, writeSleepTime: float = 0.01,
+                            numOfWriteCycles: int = 2,
+                            readSleepTime: float = 0.05, numOfRetryAttempts: int = 4, retrySleepTime: int = 0) \
+        -> (bool, list[int]):
     assert ioavservice is not None, "Are you dumb?"
 
-    dataAddress = ARM64_DDC_DATA_ADDRESS
     success = False
-    reply = []
 
-    packet = [0x80 | len(send)+1, len(send)]
+    packet = [0x80 | len(send) + 1, len(send)]
     for snd in send:
         packet.append(snd)
-    packet.append(0) # per comments in Arm64DDC.swift: the last byte is the place of the checksum, see next line!
-    packet[-1] = checksum(ARM64_DDC_7BIT_ADDRESS << 1 if len(send) == 1 else ARM64_DDC_7BIT_ADDRESS << 1 ^ dataAddress, packet, 0, len(packet)-2)
+    packet.append(0)  # per comments in Arm64DDC.swift: the last byte is the place of the checksum, see next line!
+    packet[-1] = checksum(ARM64_DDC_7BIT_ADDRESS << 1 if len(send) == 1 else ARM64_DDC_7BIT_ADDRESS << 1 ^ dataAddress,
+                          packet, 0, len(packet) - 2)
 
-    #here is where I changed things
+    # here is where I changed things
     for _ in range(0, numOfRetryAttempts):
+        # why do we have multiple, default of 2, write cycles? Do we need this?
+        # for _ in range(0, max(numOfWriteCycles, 1)):
         time.sleep(writeSleepTime)
-        success = IOAVServiceWriteI2C(ioavservice, ARM64_DDC_7BIT_ADDRESS, dataAddress, packet, len(packet)) == 0
-        if success:
+        success = IOAVServiceWriteI2C(ioavservice, ARM64_DDC_7BIT_ADDRESS, ARM64_DDC_DATA_ADDRESS, packet, len(packet)) == 0
+        reply = []
+        if read_reply:
             time.sleep(readSleepTime)
-            ret, rep = IOAVServiceReadI2C(ioavservice, ARM64_DDC_7BIT_ADDRESS, dataAddress, None, 12)
+            ret, rep = IOAVServiceReadI2C(ioavservice, ARM64_DDC_7BIT_ADDRESS, ARM64_DDC_DATA_ADDRESS, None, read_buffer_size)
             if ret == 0:
-                chksm = checksum(0x50, rep, 0, len(rep) - 2)
-                if chksm == rep[-1]:
-                    return rep
-                reply = rep
+                success = checksum(0x50, rep, 0, len(rep) - 2) == rep[-1]
+                if success:
+                    reply = rep
+        if success:
+            return success, reply
+        time.sleep(retrySleepTime)
+
+    return success, []
 
 
 def getIORegServiceAppleCDC2Properties(entry: int) -> IOregService:
@@ -130,7 +151,8 @@ def getIORegServiceAppleCDC2Properties(entry: int) -> IOregService:
     # print(f"cpath: {cpath}")
     ioregService.ioDisplayLocation = cpath
 
-    NSDictDisplayAttrs = IORegistryEntryCreateCFProperty(entry, "DisplayAttributes", kCFAllocatorDefault, kIORegistryIterateRecursively)
+    NSDictDisplayAttrs = IORegistryEntryCreateCFProperty(entry, "DisplayAttributes", kCFAllocatorDefault,
+                                                         kIORegistryIterateRecursively)
     # print(f"NSDictDisplayAttrs: {NSDictDisplayAttrs}")
     # print(f"NSDictDisplayAttrs class: {NSDictDisplayAttrs.__class__}")
     if NSDictDisplayAttrs:
@@ -145,7 +167,8 @@ def getIORegServiceAppleCDC2Properties(entry: int) -> IOregService:
             # print(productAttributes)
             # print(type(productAttributes))
 
-    NSDictTransport = IORegistryEntryCreateCFProperty(entry, "Transport", kCFAllocatorDefault, kIORegistryIterateRecursively)
+    NSDictTransport = IORegistryEntryCreateCFProperty(entry, "Transport", kCFAllocatorDefault,
+                                                      kIORegistryIterateRecursively)
     if NSDictTransport:
         transport = Conversion.pythonCollectionFromPropertyList(NSDictTransport)
         ioregService.transportUpstream = transport.get("Upstream")
@@ -160,9 +183,9 @@ def setIORegServiceDCPAVServiceProxy(entry: int, ioregService: IOregService):
         ioregService.location = location
         if location == "External":
             ioavService = IOAVServiceCreateWithService(kCFAllocatorDefault, entry)
-            print(ioavService)
-            print(type(ioavService))
-            print(dir(ioavService))
+            # print(ioavService)
+            # print(type(ioavService))
+            # print(dir(ioavService))
             ioregService.service = ioavService
 
 
@@ -223,12 +246,8 @@ def getIoregServicesForMatching() -> list[IOregService]:
 
 
 services = getIoregServicesForMatching()
-for s in services:
-    print(s)
+for i, s in enumerate(services):
+    print(f'services[{i}]:', s)
 
-#######
-# try to get DDC services, and skip displays which error
-######
-display_ids = []
-for d in display_ids_tuple:
-    pass
+result = read(services[1].service, 96, 0.05, 10)
+print(f'read(services[1].service={services[1].service}, 96, 0.05, 10) =>', result)
