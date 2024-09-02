1803d7f99f084e18bbd2d9af70b59f5a22410ba4
Seizure1990 <seizure1990@gmail.com>
2024-09-02 00:48:51 -0400

    filled out behavior for previously unclear scenarios, but noted said behavior is undefined in real world, so not worth testing against

--- test/pyddc/vcp_dummy.py
+++ test/pyddc/vcp_dummy.py
@@ -39,8 +39,9 @@ class DummyVCP(ABCVCP):
         del timeout # unused
         code = com.value
         if code not in supported_codes:
-            # TODO: what should happen when an unsupported VCP command is sent?
-            pass
+            # This is not defined behavior in practice, some monitors don't complain.
+            # Therefor it is probably not worth testing against this result.
+            raise OSError("The monitor does not support the specified VCP code.")
         if supported_codes[code] is None:
             if value > unknown_max_values[code]:
                 raise ValueError(f"value of {value} exceeds code maximum of {unknown_max_values[code]} for {VCPCommand.name}")
@@ -50,20 +51,23 @@ class DummyVCP(ABCVCP):
             if value in supported_codes[code]:
                 current_values[code] = value
             else:
-                # TODO: What happens if an unsupported value is sent? Presumably nothing but should check
-                pass
+                # This is not defined behavior in practice, some monitors don't complain.
+                # Therefor it is probably not worth testing against this result.
+                raise OSError("The monitor does not support the specified VCP code.")
 
     def _get_vcp_feature(self, com: VCPCommand, timeout: float) -> (int, int):
         del timeout # unused
         code = com.value
         if code not in supported_codes:
-            # TODO: what should happen when an unsupported VCP command is sent?
-            pass
+            # This is not defined behavior in practice, some monitors don't complain and just return garbage.
+            # Therefor it is probably not worth testing against this result.
+            raise OSError("The monitor does not support the specified VCP code.")
         if not com.discrete:
             maxv = unknown_max_values[code]
         else:
+            # The max value on discrete features is the number of options, but there's nothing meaningful to
+            #   test in that regard, and MonitorBoss doesn't use that info, so not worth worrying about
             maxv = 0
-            # TODO: what gets sent back as max for discreet commands?
         return VCPFeatureReturn(current_values[code], maxv)
 
     def _get_vcp_capabilities_str(self, timeout: float) -> str:
