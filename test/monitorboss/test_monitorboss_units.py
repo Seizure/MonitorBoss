a9ceedfabd19a6dbd0f6dcbc643a3d0fc782f064
Seizure1990 <seizure1990@gmail.com>
2024-09-24 02:58:51 -0400

    reorganized info.* unit tests

--- test/monitorboss/test_monitorboss_units.py
+++ test/monitorboss/test_monitorboss_units.py
@@ -120,7 +120,7 @@ class TestCLIcheckers:
             assert "CONFIG ALIASES" not in output.err
 
 
-class TestInfoData:
+class TestInfoFeatureData:
 
     def test_FeatureData_serialize_yesaliases(self):
         com = get_vcp_com(VCPCodes.input_source)
@@ -133,6 +133,15 @@ class TestInfoData:
         data = info.FeatureData(com.name, com.code, tuple())
         assert data.serialize() == {"name": "input_source", "code": 96}
 
+    def test_FeatureData_str_found_com(self):
+        com = get_vcp_com(VCPCodes.input_source)
+        data = info.FeatureData(com.name, com.code, tuple())
+        assert data.__str__() == f"{com.name} ({com.code})"
+
+    def test_FeatureData_str_no_com(self):
+        # TODO: test for if no known com
+        pass
+
     def test_feature_data_found_com(self, test_cfg):
         code = VCPCodes.input_source
         com = get_vcp_com(code)
@@ -143,17 +152,7 @@ class TestInfoData:
         # TODO: stub - test when code is not known com
         pass
 
-    def test_MonitorData_yesserialize(self):
-        data = info.MonitorData(1, tuple(['foo', 'bar', 'baz']))
-        assert data.serialize() == {"id": 1, "aliases": ('foo', 'bar', 'baz')}
-
-    def test_MonitorData_serialize_noaliases(self):
-        data = info.MonitorData(1, tuple())
-        assert data.serialize() == {"id": 1}
-
-    def test_monitor_data(self, test_cfg):
-        data = info.MonitorData(1, tuple(['bar', 'baz']))
-        assert info.monitor_data(1, test_cfg) == data
+class TestInfoValueData:
 
     def test_ValueData_serialize_yesparam_yesaliases(self):
         data = info.ValueData(75, 'foo', tuple(['bar', 'baz']))
@@ -171,6 +170,22 @@ class TestInfoData:
         data = info.ValueData(75, '', tuple())
         assert data.serialize() == {"value": 75}
 
+    def test_ValueData_str_noparam_noalias(self):
+        data = info.ValueData(24, "", tuple())
+        assert data.__str__() == "24"
+
+    def test_ValueData_str_yesparam_noalias(self):
+        data = info.ValueData(24, "foo", tuple())
+        assert data.__str__() == "24 (PARAM: foo)"
+
+    def test_ValueData_str_noparam_yesalias(self):
+        data = info.ValueData(24, "", tuple(['foo', 'bar']))
+        assert data.__str__() == "24 (ALIASES: foo, bar)"
+
+    def test_ValueData_str_yesparam_yesalias(self):
+        data = info.ValueData(24, "foo", tuple(["bar", "baz"]))
+        assert data.__str__() == "24 (PARAM: foo | ALIASES: bar, baz)"
+
     def test_value_data_found_com(self, test_cfg):
         code = VCPCodes.input_source
         data = info.ValueData(17, "hdmi1", tuple(["hdmi"]))
@@ -180,6 +195,34 @@ class TestInfoData:
         # TODO: stub - test when code is not known com
         pass
 
+class TestInfoMonitorData:
+
+    def test_MonitorData_yesserialize(self):
+        data = info.MonitorData(1, tuple(['foo', 'bar', 'baz']))
+        assert data.serialize() == {"id": 1, "aliases": ('foo', 'bar', 'baz')}
+
+    def test_MonitorData_serialize_noaliases(self):
+        data = info.MonitorData(1, tuple())
+        assert data.serialize() == {"id": 1}
+
+    def test_MonitorData_str_noalias(self):
+        data = info.MonitorData(2, tuple())
+        assert data.__str__() == "monitor #2"
+
+    def test_MonitorData_str_onealias(self):
+        data = info.MonitorData(0, tuple(["foo"]))
+        assert data.__str__() == "monitor #0 (foo)"
+
+    def test_MonitorData_str_multialias(self):
+        data = info.MonitorData(1, tuple(["bar", "baz"]))
+        assert data.__str__() == "monitor #1 (bar, baz)"
+
+    def test_monitor_data(self, test_cfg):
+        data = info.MonitorData(1, tuple(['bar', 'baz']))
+        assert info.monitor_data(1, test_cfg) == data
+
+class TestInfoCapabilitydata:
+
     def test_CapabilityData_serialize_no_errata(self):
         # TODO: stub
         pass
@@ -188,50 +231,11 @@ class TestInfoData:
         # TODO: stub
         pass
 
-    def test_capability_data(self):
+    def test_CapabilityData_str(self):
         # TODO: stub
         pass
 
 
-class TestInfoStr:
-
-    def test_feature_str_found_com(self):
-        com = get_vcp_com(VCPCodes.input_source)
-        data = info.FeatureData(com.name, com.code, tuple())
-        assert data.__str__() == f"{com.name} ({com.code})"
-
-    def test_feature_str_no_com(self):
-        # TODO: test for if no known com
-        pass
-
-    def test_monitor_str_noalias(self):
-        data = info.MonitorData(2, tuple())
-        assert data.__str__() == "monitor #2"
-
-    def test_monitor_str_onealias(self):
-        data = info.MonitorData(0, tuple(["foo"]))
-        assert data.__str__() == "monitor #0 (foo)"
-
-    def test_monitor_str_multialias(self):
-        data = info.MonitorData(1, tuple(["bar", "baz"]))
-        assert data.__str__() == "monitor #1 (bar, baz)"
-
-    def test_value_str_noparam_noalias(self):
-        data = info.ValueData(24, "", tuple())
-        assert data.__str__() == "24"
-
-    def test_value_str_yesparam_noalias(self):
-        data = info.ValueData(24, "foo", tuple())
-        assert data.__str__() == "24 (PARAM: foo)"
-
-    def test_value_str_noparam_yesalias(self):
-        data = info.ValueData(24, "", tuple(['foo', 'bar']))
-        assert data.__str__() == "24 (ALIASES: foo, bar)"
-
-    def test_value_str_yesparam_yesalias(self):
-        data = info.ValueData(24, "foo", tuple(["bar", "baz"]))
-        assert data.__str__() == "24 (PARAM: foo | ALIASES: bar, baz)"
-
-    def test_capability_str(self):
+    def test_capability_data(self):
         # TODO: stub
         pass
